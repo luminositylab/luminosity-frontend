0b44a6906af38d7cd40e82a34b6cf349775622ef
Chase Adams <c@cadams.io>
2019-09-17 10:13:48 -0700

    feat: added BioModal for team members

--- src/pages/team.js
+++ src/pages/team.js
@@ -9,103 +9,108 @@ import { leadership, members } from '../content/members';
 
 import teamWorkVideo from '../assets/videos/team-work-blurry.mp4';
 
-class Team extends React.Component {
-  state = {
-    isBioOpen: false,
-    member: {}
-  };
-
-  createHeadshot = member => (
-    <div
-      onClick={this.openBioPage(member)}
-      key={member.name}
-      className="container"
-    >
-      <div className="image">
-        {member.image && (
-          <Img
-            imgStyle={{ objectFit: 'cover' }}
-            fluid={member.image}
-            alt={member.name}
-          />
-        )}
-      </div>
-      <div className="content">
-        <div>
-          <header>
-            <h2>{member.name}</h2>
-          </header>
-          <p>{member.title}</p>
-        </div>
+const createHeadshot = ({ member, onClick }) => (
+  <div
+    onClick={onClick}
+    key={member.name}
+    className="container"
+  >
+    <div className="image">
+      {member.image && (
+        <Img
+          imgStyle={{ objectFit: 'cover' }}
+          fluid={member.image}
+          alt={member.name}
+        />
+      )}
+    </div>
+    <div className="content">
+      <div>
+        <header>
+          <h2>{member.name}</h2>
+        </header>
+        <p>{member.title}</p>
       </div>
     </div>
+  </div>
+);
+
+const Team = ({ data, location }) => {
+  const [bio, setBio] = React.useState(null);
+
+  const mappedPathsToImages = data.headshotImages.edges.reduce(
+    (prev, curr) => ({
+      ...prev,
+      [curr.node.name]: curr.node.childImageSharp.fluid
+    }),
+    {}
   );
 
-  openBioPage = member => () => {
-    this.setState({ isBioOpen: true, member });
-  };
+  return (
+    <Layout>
+      <SEO
+        title="The Team"
+        description="The team which composes the Luminosity Lab."
+        location={location}
+      />
 
-  handleClose = () => {
-    this.setState({ isBioOpen: false });
-  };
+      <Banner
+        title={'The Team'}
+        description={
+          'We are made up of brilliant people from around the world.'
+        }
+        video={teamWorkVideo}
+      />
 
-  render() {
-    const { data, location } = this.props;
+      <BioModal
+        open={Boolean(bio)}
+        title={bio && bio.title}
+        description={bio && bio.description}
+        handleClose={() => setBio(null)}
+      />
 
-    const mappedPathsToImages = data.headshotImages.edges.reduce(
-      (prev, curr) => ({
-        ...prev,
-        [curr.node.name]: curr.node.childImageSharp.fluid
-      }),
-      {}
-    );
-
-    return (
-      <Layout>
-        <SEO
-          title="The Team"
-          description="The team which composes the Luminosity Lab."
-          location={location}
-        />
-
-        <Banner
-          title={'The Team'}
-          description={
-            'We are made up of brilliant people from around the world.'
-          }
-          video={teamWorkVideo}
-        />
-
-        <div id="main">
-          <section id="one" className="headshots">
-            <BioModal {...this.state} handleClose={this.handleClose} />
-            <div className="title">Leadership</div>
-            <div className="members">
-              {leadership.map(e =>
-                this.createHeadshot({
+      <div id="main">
+        <section id="one" className="headshots">
+          <div className="title">Leadership</div>
+          <div className="members">
+            {leadership.map(e =>
+              createHeadshot({
+                member: {
                   ...e,
                   image: mappedPathsToImages[e.path]
-                })
-              )}
-            </div>
-          </section>
-          <section id="two" className="headshots">
-            <div className="title">Students</div>
-            <div className="members">
-              {members.map(e =>
-                this.createHeadshot({
+                },
+                onClick: () =>
+                  setBio({
+                    title: e.name,
+                    description: e.description
+                  })
+              })
+            )}
+          </div>
+        </section>
+        <section id="two" className="headshots">
+          <div className="title">Students</div>
+          <div className="members">
+            {members.map(e =>
+              createHeadshot({
+                member: {
                   ...e,
                   image: mappedPathsToImages[e.path]
-                })
-              )}
-            </div>
-            <div className="subtitle">...and many more!</div>
-          </section>
-        </div>
-      </Layout>
-    );
-  }
-}
+                },
+                onClick: () =>
+                  setBio({
+                    title: e.name,
+                    description: e.description
+                  })
+              })
+            )}
+          </div>
+          <div className="subtitle">...and many more!</div>
+        </section>
+      </div>
+    </Layout>
+  );
+};
 
 export default Team;
 
