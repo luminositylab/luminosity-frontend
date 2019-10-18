db0fd4beb9ff017c1194a248fc0d2794eee27b5f
Mark Naufel <mnaufel@asu.edu>
2019-10-18 01:56:48 -0700

    Fix Points

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,8 +26,8 @@ class HomeIndex extends React.Component {
       el: this.myRef.current,
       color: 0xffffff,
       backgroundColor: 0x000000,
-      points: 5,
-      maxDistance: 15
+      points: 9,
+      maxDistance: 18
     })
   }
   componentWillUnmount() {
