c28c524729242eb2eda6696b94bda6570bfa40f2
Chase Adams <c@cadams.io>
2020-02-11 09:54:53 -0700

    fix: patch fix for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,8 @@ import logo from '../assets/images/logo-with-text.png';
 const Header = props => (
   <header id="header" className="alt">
     <Helmet defer={false}>
-      <script src="https://cdn.jsdelivr.net/gh/tengbao/vanta/vendor/three.r92.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/gh/tengbao/vanta/dist/vanta.net.min.js"></script>
+      <script src="https://raw.githubusercontent.com/mrdoob/three.js/dev/build/three.min.js"></script>
+      <script src="https://raw.githubusercontent.com/tengbao/vanta/master/dist/vanta.net.min.js"></script>
     </Helmet>
     <Link to="/" className="logo">
       <img src={logo} height={40} alt="The Luminosity Lab logo" />
